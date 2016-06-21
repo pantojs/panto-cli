6d3ccf741c8fa5a04fdf14080a596696be36587c
yinyong <yanni4night@gmail.com>
2016-06-21 17:49:32 +0800

    complete

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+  * Copyright (C) 2016 pantojs.xyz
+  * index.js
+  *
+  * changelog
+  * 2016-06-21[17:47:56]:revised
+  *
+  * @author yanni4night@gmail.com
+  * @version 1.0.0
+  * @since 1.0.0
+  */
+'use strict';
+
+console.log('panto-cli is coming soon');

21b942091e1dd6a2d1512cd3e4704e87c439db4b
yinyong <yanni4night@gmail.com>
2016-06-22 00:02:00 +0800

    demo

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 /**
-  * Copyright (C) 2016 pantojs.xyz
-  * index.js
-  *
-  * changelog
-  * 2016-06-21[17:47:56]:revised
-  *
-  * @author yanni4night@gmail.com
-  * @version 1.0.0
-  * @since 1.0.0
-  */
+ * Copyright (C) 2016 pantojs.xyz
+ * index.js
+ *
+ * changelog
+ * 2016-06-21[17:47:56]:revised
+ *
+ * @author yanni4night@gmail.com
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 'use strict';
 
-console.log('panto-cli is coming soon');
+const panto = require('../panto/');
+
+panto.pick('**/*.{js,jsx}').pipe(panto.read()).end();
+
+panto.build().then((...args) => console.log(...args), (err) => console.error(err));
